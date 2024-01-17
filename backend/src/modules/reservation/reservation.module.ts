d77a7d666bcfa819753e1dff4b562b135d686b32
Ranjithprabhu <ranjithprabhu.k@gmail.com>
2024-01-17 23:27:53 +0530

    fix: inject the required dependencies

--- backend/src/modules/reservation/reservation.module.ts
+++ backend/src/modules/reservation/reservation.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ReservationService } from './reservation.service';
 import { ReservationController } from './reservation.controller';
+import { UserModule } from '../user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Reservation } from './entities/reservation.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
+  imports: [UserModule, TypeOrmModule.forFeature([Reservation])],
   controllers: [ReservationController],
-  providers: [ReservationService],
+  providers: [ReservationService, JwtService],
 })
 export class ReservationModule {}
